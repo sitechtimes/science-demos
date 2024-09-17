514d365b98f098d6c26fec2dfffa442e901f3271
LeoDwi <leod13579@gmail.com>
2024-09-17 10:31:17 -0400

    Create SliderInput.vue

---
+++ frontend/src/components/input_components/SliderInput.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Slider from "primevue/slider";
+
+let placeholderMaxDataValue = 100;
+</script>
+
+<template>
+  <div>
+    <Slider v-model="value" :step="placeholderMaxDataValue" class="w-56" />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
