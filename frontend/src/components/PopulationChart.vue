f4900934109794928a5cce94bcddc0e69fd9d9e7
angelinali96 <136626552+angelinali96@users.noreply.github.com>
2024-09-17 11:01:19 -0400

    add props for chartinfo
    maybe need to be vmodels instead

--- frontend/src/components/PopulationChart.vue
+++ frontend/src/components/PopulationChart.vue
@@ -1,11 +1,77 @@
 <template>
-    <div>
-
+    <div class="card">
+        <Chart type="line" :data="chartData" :options="chartOptions" class="h-[30rem]" />
     </div>
 </template>
 
 <script setup>
+import Chart from 'primevue/chart'
+import { ref, onMounted } from "vue";
 
+const chartInfo = defineProps({
+    chartData: Object,
+    chartTitle: String,
+    xAxis: String,
+    yAxis: String
+})
+
+onMounted(() => {
+    chartData.value = setChartData();
+    chartOptions.value = setChartOptions();
+});
+
+const chartData = ref();
+const chartOptions = ref();
+        
+const setChartData = () => {
+    return chartInfo.chartData;
+};
+const setChartOptions = () => {
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--p-text-color');
+    const textColorSecondary = documentStyle.getPropertyValue('--p-text-muted-color');
+    const surfaceBorder = documentStyle.getPropertyValue('--p-content-border-color');
+
+    return {
+        maintainAspectRatio: false,
+        aspectRatio: 0.6,
+        plugins: {
+            legend: {
+                labels: {
+                    color: textColor
+                }
+            }
+        },
+        scales: {
+            x: {
+                ticks: {
+                    color: textColorSecondary
+                },
+                grid: {
+                    color: surfaceBorder
+                },
+                title: {
+                    display: true,
+                    text: chartInfo.xAxis,
+                    color: textColor
+                }
+            },
+            y: {
+                ticks: {
+                    color: textColorSecondary
+                },
+                grid: {
+                    color: surfaceBorder
+                },
+                title: {
+                    display: true,
+                    text: chartInfo.yAxis,
+                    color: textColor
+                }
+            }
+        }
+    };
+}
 </script>
 
 <style scoped>
--- frontend/src/views/pages/apes/AbioticCoral.vue
+++ frontend/src/views/pages/apes/AbioticCoral.vue
@@ -1,11 +1,34 @@
 <template>
     <div>
-
+        <PopulationChart :chartData="data" :xAxis="'Years'" :yAxis="'Percent of Population Remaining'"/>
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+import PopulationChart from '../../../components/PopulationChart.vue';
 
+const documentStyle = getComputedStyle(document.documentElement);
+
+const data = ref({
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+            {
+                label: 'First Dataset',
+                data: [65, 59, 80, 81, 56, 55, 40],
+                fill: false,
+                borderColor: documentStyle.getPropertyValue('--p-cyan-500'),
+                tension: 0.4
+            },
+            {
+                label: 'Second Dataset',
+                data: [28, 48, 40, 19, 86, 27, 90],
+                fill: false,
+                borderColor: documentStyle.getPropertyValue('--p-gray-500'),
+                tension: 0.4
+            }
+        ]
+    });
 </script>
 
 <style scoped>
