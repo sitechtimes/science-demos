bd1ac1f9477296871db14478a17497c8737c4089
LeoDwi <leod13579@gmail.com>
2024-09-18 10:45:54 -0400

    Update CalculationStore.js

--- frontend/src/Stores/CalculationStore.js
+++ frontend/src/Stores/CalculationStore.js
@@ -1,3 +1,7 @@
 import { defineStore } from "pinia";
+import { DataStore } from "./DataStore";
 
-export const CalculationStore = defineStore("calculation", () => {});
+export const CalculationStore = defineStore("calculation", () => {
+  const calculatedData = ref([]);
+  const years = ref([]);
+});
