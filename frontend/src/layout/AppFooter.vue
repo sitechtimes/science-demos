04a0389afffb9b9128c0e5845afce520ffbb6f0c
lawrence <91681777+whydoesthecodenotwork@users.noreply.github.com>
2024-09-19 10:34:42 -0400

    Update AppFooter.vue

--- frontend/src/layout/AppFooter.vue
+++ frontend/src/layout/AppFooter.vue
@@ -1,8 +1,7 @@
 <script setup></script>
 
 <template>
-    <div class="layout-footer">
-        SITHS Science by
-        <p>Leo Dwi, Veronica Frid, Samuel Kipnis & Angelina Li</p>
-    </div>
+  <div class="layout-footer">
+    <p>SITHS Science by Leo Dwi, Veronica Frid, Samuel Kipnis & Angelina Li</p>
+  </div>
 </template>
