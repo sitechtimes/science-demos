23844befde46a2fd19f546a244beadb7da5641cf
Vfrid <113192881+Vfrid@users.noreply.github.com>
2024-09-20 11:01:01 -0400

    main function

--- frontend/src/components/beta_formulas2.vue
+++ frontend/src/components/beta_formulas2.vue
@@ -77,44 +77,93 @@ const past_yellowtail_snapper=ref([240])
 const past_red_lionfish=ref([0])
 const past_crown_of_thorns_starfish=ref([0])
 
-function calculate_animal(condition, reg_condition, animal, reg_animal, list){
+// function calculate_animal(condition, reg_condition, animal, reg_animal, list){
+//     const condition_difference = condition-reg_condition
+//     let result
+//     if (list.length === 1){
+//         console.log('list length 1')
+//         if (condition_difference<0){
+//             result = animal * (((condition_difference-(condition_difference*2))/100)+1)
+//         }
+//         else if(condition_difference === 0){
+//             result = animal
+//         }
+//         else{
+//             result = animal * (condition_difference/100)
+//         }
+//     }
+//     else{
+//         if (condition_difference<0){
+//             result = list[list.length-1] * (((condition_difference-(condition_difference*2))/100)+1)
+//         }
+//         else if(condition_difference === 0){
+//             result = list[list.length-1]
+//         }
+//         else{
+//             result = list[list.length-1] * (condition_difference/100)
+//         }
+//     }
+//     list.push(result)
+//     return result
+// }
+function find_mult_value(condition,reg_condition){
     const condition_difference = condition-reg_condition
     let result
-    if (list.length === 1){
-        console.log('list length 1')
-        if (condition_difference<0){
-            result = animal * (((condition_difference-(condition_difference*2))/100)+1)
+    if (condition_difference<0){
+            result =  (((condition_difference-(condition_difference*2))/100)+1)
         }
         else if(condition_difference === 0){
-            result = animal
+            result = false //no change, tell to ignore if value is 0
         }
         else{
-            result = animal * (condition_difference/100)
+            result = (condition_difference/100)
         }
-    }
-    else{
-        if (condition_difference<0){
-            result = list[list.length-1] * (((condition_difference-(condition_difference*2))/100)+1)
-        }
-        else if(condition_difference === 0){
-            result = list[list.length-1]
-        }
-        else{
-            result = list[list.length-1] * (condition_difference/100)
-        }
-    }
-    list.push(result)
     return result
 }
-function calc_algae(condition,reg_condition,animal,reg_animal, list){
-    console.log("Storm severity = "+ conditions.value[0].storm_severity)
-    animals.value[0].algae = calculate_animal(condition,reg_condition,animal,reg_animal, list)
-    console.log("Algae = "+ animals.value[0].algae)
-    console.log(past_algae.value)
+function calc_animal(mult_list, list){
+    let result
+    list.push(list[list.length-1])
+    for (let i = 0; i<mult_list.length; i++){
+        if(mult_list[i]===false){
+            continue
+        }
+        else{
+            list[list.length-1]=mult_list[i]*list[list.length-2]
+        }
+    }
+    return list[list.length-1]
 }
 
 function calculate_everything(){
-    calc_algae(conditions.value[0].storm_severity,reg_conditions[0].storm_severity,animals.value[0].algae,reg_animals[0].reg_algae, past_algae.value)
+    const storm = find_mult_value(conditions.value[0].storm_severity, reg_conditions[0].storm_severity)
+    const temp = find_mult_value(conditions.value[0].ocean_temp, reg_conditions[0].ocean_temp)
+    const ph = find_mult_value(conditions.value[0].ocean_ph, reg_conditions[0].ocean_ph)
+    const logging = find_mult_value(conditions.value[0].logging, reg_conditions[0].logging)
+    const sewage = find_mult_value(conditions.value[0].sewage, reg_conditions[0].sewage)
+    const agriculture = find_mult_value(conditions.value[0].agriculture, reg_conditions[0].agriculture)
+    const fishing = find_mult_value(conditions.value[0].net_fishing, reg_conditions[0].net_fishing)
+    const grouper = find_mult_value(conditions.value[0].grouper, reg_conditions[0].grouper)
+    const snapper = find_mult_value(conditions.value[0].snapper, reg_conditions[0].snapper)
+    const black = find_mult_value(conditions.value[0].black_band,reg_conditions[0].black_band)
+    const white = find_mult_value(conditions.value[0].white_band,reg_conditions[0].white_band)
+    const urchin = find_mult_value(conditions.value[0].sea_urchin_infection,reg_conditions[0].sea_urchin_infection)
+    const lionfish = find_mult_value(conditions.value[0].red_lionfish_invasive,reg_conditions[0].red_lionfish_invasive)
+    const thorns = find_mult_value(conditions.value[0].crown_of_thorns_invasive,reg_conditions[0].crown_of_thorns_invasive)
+    const mult_list = [storm, temp, ph, logging, sewage, agriculture, fishing, grouper, snapper, black, white, urchin, lionfish, thorns]
+    console.log(mult_list)
+    animals.value[0].staghorn_coral=calc_animal(mult_list, past_staghorn_coral.value)
+    animals.value[0].boulder_star_coral=calc_animal(mult_list, past_boulder_star_coral.value)    
+    animals.value[0].algae=calc_animal(mult_list, past_algae.value)
+    animals.value[0].long_spined_urchin=calc_animal(mult_list, past_long_spined_urchin.value)
+    animals.value[0].sponge=calc_animal(mult_list, past_sponge.value)
+    animals.value[0].queen_angelfish=calc_animal(mult_list, past_queen_angelfish.value)
+    animals.value[0].nassau_grouper=calc_animal(mult_list, past_nassau_grouper.value)
+    animals.value[0].hawksbill_sea_turtle=calc_animal(mult_list, past_hawksbill_sea_turtle.value)
+    animals.value[0].spotlight_parrotfish=calc_animal(mult_list, past_spotlight_parrotfish.value)
+    animals.value[0].yellowtail_snapper=calc_animal(mult_list, past_yellowtail_snapper.value)
+    animals.value[0].red_lionfish=calc_animal(mult_list, past_red_lionfish.value)    
+    animals.value[0].crown_of_thorns_starfish=calc_animal(mult_list, past_crown_of_thorns_starfish.value)
+    console.log(animals.value[0])
 }
 </script>
 <template>
