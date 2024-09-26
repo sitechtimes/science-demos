97ec202af3de1426f8ce8abae1c16c4464c139cc
LeoDwi <leod13579@gmail.com>
2024-09-26 10:55:41 -0400

    added a v-for for sliders, still gotta fix child editing store

--- frontend/src/components/SliderContainer.vue
+++ frontend/src/components/SliderContainer.vue
@@ -1,17 +1,27 @@
 <script setup>
 import SliderInput from "./input_components/SliderInput.vue";
 import { DataStore } from "../Stores/DataStore";
+import { ref } from "vue";
 
 const dataStore = DataStore();
+const sliderVariables = ref([
+  dataStore.oceanTemp,
+  dataStore.stormSeverity,
+  dataStore.WaterPH,
+]);
 </script>
 
 <template>
   <div>
-    <SliderInput
-      :storeValue="dataStore.oceanTemp.sliderValue"
-      :sliderSteps="dataStore.oceanTemp.sliderStep"
-      :minSliderValue="dataStore.oceanTemp.sliderMax"
-      :maxSliderValue="dataStore.oceanTemp.sliderMin"
-    />
+    <div v-for="variable in sliderVariables" :key="variable">
+      <InputText v-model.number="variable.sliderValue" />
+      <SliderInput
+        :key="variable"
+        :storeValue="variable.sliderValue"
+        :sliderSteps="variable.sliderStep"
+        :minSliderValue="variable.sliderMax"
+        :maxSliderValue="variable.sliderMin"
+      />
+    </div>
   </div>
 </template>
