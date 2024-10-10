463619a774a108f704fdc7ca21f0d6ca11c8d5a7
Samuel Kipnis <sam.kipnis@gmail.com>
2024-10-10 11:03:06 -0400

    images & descriptions

---
+++ frontend/src/games/CoralSimulation/organisms.js
@@ -0,0 +1,51 @@
+const path = '/src/games/CoralSimulatio/images/'
+
+export default {
+    algae: {
+        img: path + 'algae.png'
+    },
+    boulder_star_coral: {
+        img: path + 'boulder_star_coral.png',
+        description: 'boulder star coral'
+    },
+    crown_of_thorns_starfish: {
+        img: path + 'crown_of_thorns_starfish.png',
+        description:'crown of thorns'
+    },
+    hawksbill_sea_turtle: {
+        img: path + 'hawksbill_sea_turtle.png',
+        description: 'hawksbill sea turtle'
+    },
+    long_spined_urchin: {
+        img: path + 'long_spined_urchin.png',
+        description: 'long spined urchin'
+    },
+    nassau_grouper: {
+        img: path + 'nassau_grouper.png',
+        description: 'nassau grouper'
+    },
+    queen_angelfish: {
+        img: path + 'queen_angelfish.png',
+        description: 'queen angelfish'
+    },
+    red_lionfish: {
+        img: path + 'red_lionfish.png',
+        description: 'red lionfish'
+    },
+    sponge: {
+        img: path + 'sponge.png',
+        description:  'sponge'
+    },
+    spotlight_parrotfish: {
+        img: path + 'spotlight_parrotfish.png',
+        description: 'spotlight parrotfish'
+    },
+    staghorn_coral: {
+        img: path + 'staghorn_coral.png',
+        description: 'staghorn coral'
+    },
+    yellowtail_snapper: {
+        img: path + 'yellowtail_snapper.png',
+        description: 'yellowtail snapper'
+    },
+}
