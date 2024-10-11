85d0bb8c45aaf03a28cce31ed85049c5dbcfbb48
LeoDwi <leod13579@gmail.com>
2024-10-11 10:48:59 -0400

    changed calculated results to carbon store

--- frontend/src/components/coral/SliderConditions.vue
+++ frontend/src/components/coral/SliderConditions.vue
@@ -1,11 +1,11 @@
 <script setup>
 import SliderInput from "../input_components/SliderInput.vue";
 import { DataStore } from "@/Stores/DataStore";
-import { CalculatedResultsStore } from "@/Stores/CarbonStore";
+import { CarbonStore } from "@/Stores/CarbonStore";
 import { ref } from "vue";
 
 const dataStore = DataStore();
-const calculatedResultsStore = CalculatedResultsStore();
+const carbonStore = CarbonStore();
 const sliderVariables = ref(["oceanTemp", "stormSeverity", "waterPH"]);
 // if we do include more conditions we will have to include a selectbutton or dropdown to switch between
 </script>
@@ -16,6 +16,6 @@ const sliderVariables = ref(["oceanTemp", "stormSeverity", "waterPH"]);
       <SliderInput :var="dataStore[variable]" />
     </div>
     <!-- return to original settings button here? will need store to have default value -->
-    {{ calculatedResultsStore.temperatureKelvins }}
+    {{ carbonStore.temperatureKelvins }}
   </div>
 </template>
