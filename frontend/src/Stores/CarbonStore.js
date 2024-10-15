a6c9cb3cd90f4311bb545d30778a5cc703b5d5a5
LeoDwi <leod13579@gmail.com>
2024-10-15 10:49:42 -0400

    coded some math, reorganizing some stuff

--- frontend/src/Stores/CarbonStore.js
+++ frontend/src/Stores/CarbonStore.js
@@ -4,23 +4,32 @@ import { defineStore } from "pinia";
 import { DataStore } from "./DataStore";
 import { ref, computed } from "vue";
 
-export const CarbonStore = defineStore("calculated", () => {
-  const dataStore = DataStore();
+const dataStore = DataStore();
 
-  const partialPressureCO2 = ref();
+export const CarbonStore = defineStore("calculated", () => {
+  //computed properties. They are read only and cannot be changed.
   const temperatureKelvins = computed(
     () => dataStore.oceanTemp.sliderValue + 273
   );
+  const salinityCoefficient = computed(
+    () => 0.0433 - 2.67 * Math.pow(10, -4) * temperatureKelvins
+  );
+  const salinityFactor = computed(
+    () => 1 + salinityCoefficient * dataStore.waterSalinity.sliderValue
+  );
+  const hydrogenIons = computed(() => Math.pow(10, -acidity));
+
+  //variables that need to be changed during calc?
+  const partialPressureCO2 = ref();
   const carbonConcentration = ref();
-  const salinityCoefficient = ref();
-  const henrysConstant = ref();
+  const henrysConstant = ref(); //prob can be computed, will code it out later
   const acidity = ref();
-  const hydrogenIons = ref();
 
   return {
     partialPressureCO2,
     temperatureKelvins,
     carbonConcentration,
+    salinityFactor,
     salinityCoefficient,
     henrysConstant,
     acidity,
