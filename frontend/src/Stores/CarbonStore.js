9f38b3b75f8d4a6b6f18d5f7ff178c23f1c9a333
LeoDwi <leod13579@gmail.com>
2024-10-28 10:33:34 -0400

    erm

--- frontend/src/Stores/CarbonStore.js
+++ frontend/src/Stores/CarbonStore.js
@@ -7,6 +7,8 @@ import { ref, computed } from "vue";
 const dataStore = DataStore();
 
 export const CarbonStore = defineStore("calculated", () => {
+  const partialPressureCO2 = ref(356.8269697);
+
   //computed properties. They are read only and cannot be changed.
   const temperatureKelvins = computed(
     () => dataStore.oceanTemp.sliderValue + 273
@@ -17,22 +19,46 @@ export const CarbonStore = defineStore("calculated", () => {
   const salinityFactor = computed(
     () => (1 + salinityCoefficient.value) * dataStore.waterSalinity.sliderValue
   );
-  const henrysConstant = computed(
-    () =>
-      salinityFactor.value *
-      12.59 *
-      Math.pow(temperatureKelvins.value, -1.14) *
-      Math.pow(10, -2700 / temperatureKelvins.value)
-  );
-  const carbonConcentration = computed(
-    () => henrysConstant.value * tempPartialPressureCO2.value
-  );
-  const acidity = computed(() => 6.37 + carbonConcentration.value / 0.00813566);
-  const hydrogenIons = computed(() => Math.pow(10, -acidity.value));
+  // const henrysConstant = computed(
+  //   () =>
+  //     salinityFactor.value *
+  //     12.59 *
+  //     Math.pow(temperatureKelvins.value, -1.14) *
+  //     Math.pow(10, -2700 / temperatureKelvins.value)
+  // );
 
-  //variables that need to be changed during calc?
-  const partialPressureCO2 = ref(356.8269697);
-  const tempPartialPressureCO2 = ref(100);
+  const henrysConstant = computed(() => {
+    const T = temperatureKelvins.value;
+    const S = dataStore.waterSalinity.sliderValue;
+
+    const KH = Math.exp(
+      9345.17 / T -
+        60.2409 +
+        23.3585 * Math.log(T / 100) +
+        S * (0.023517 - 0.00023656 * T + 0.0047036 * Math.pow(T / 100, 2))
+    );
+
+    console.log("Henry's constant (KH):", KH);
+    return KH;
+  });
+
+  const carbonConcentration = computed(
+    () => henrysConstant.value * (partialPressureCO2.value / 1e6)
+  );
+  const acidity = computed(() => {
+    console.log("carbon =", carbonConcentration.value);
+    const pK1 = 6.37;
+    const K1 = Math.pow(10, -6.37);
+    const pK2 = 10.33;
+    const K2 = Math.pow(10, -pK2);
+    const bicarbonateConcentration =
+      (K1 * carbonConcentration.value) / (K2 + carbonConcentration.value);
+    console.log("bicarb", bicarbonateConcentration);
+    return (
+      pK1 + Math.log10(bicarbonateConcentration / carbonConcentration.value)
+    );
+  });
+  const hydrogenIons = computed(() => Math.pow(10, -acidity.value));
 
   return {
     partialPressureCO2,
@@ -43,6 +69,5 @@ export const CarbonStore = defineStore("calculated", () => {
     henrysConstant,
     acidity,
     hydrogenIons,
-    tempPartialPressureCO2,
   };
 });
