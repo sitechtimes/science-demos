d1f746ff630af43f3fd01908b8c4b6d74f6d0068
Vfrid <113192881+Vfrid@users.noreply.github.com>
2024-10-31 10:58:36 -0400

    'missing' error, ill fix on monday

--- frontend/src/games/CellDivisionSimulation/scenes/Game.js
+++ frontend/src/games/CellDivisionSimulation/scenes/Game.js
@@ -1,6 +1,7 @@
 import { EventBus } from '../EventBus';
 import {Scene} from 'phaser';
-import Cell from '../Cell'
+import Cell from '../Cell';
+import Textbox from '../Cell'
 import {ref} from 'vue'
 
 export class Game extends Scene
@@ -13,58 +14,60 @@ export class Game extends Scene
     }
 
     progressYear(){
-
-        if (this.time_in_cycle === 0){
-            //dissolve nuclear membrane
-            this.time_in_cycle++;
-            this.year++
-            this.cells.forEach((cell)=>cell.setTexture('time1'))
-            this.cells.forEach((cell)=>cell.clearPopup())
-        }
-        else if (this.time_in_cycle === 1){
-            //centrosomes attach to 
-            // this.cell.setTexture('time1')
-            this.time_in_cycle++;
-            this.year++
-            this.cells.forEach((cell)=>cell.setTexture('time2'))
-            this.cells.forEach((cell)=>cell.clearPopup())
-        }
-        else if(this.time_in_cycle === 2){
-            //centrosomes pull apart chromosomes, cell squishes in middle
-            this.time_in_cycle++;
-            this.year++
-            this.cells.forEach((cell)=>cell.setTexture('time3'))
-            this.cells.forEach((cell)=>cell.clearPopup())
-        }
-        else if(this.time_in_cycle===3){
-            //nuclear membrane develops around each set of chromosomes, cell squishes more
-            this.time_in_cycle++;
-            this.year++
-            this.cells.forEach((cell)=>cell.setTexture('time4'))
-            this.cells.forEach((cell)=>cell.clearPopup())
-        }
-        else if(this.time_in_cycle===4){
-            //cell separates
-            this.time_in_cycle++;
-            this.year++
-            this.cells.forEach((cell)=>cell.setTexture('time5'))
-            this.cells.forEach((cell)=>cell.clearPopup())
-        }
-        else{
-            this.time_in_cycle=0;
-            this.year++
-            this.cells.forEach((cell)=>cell.setTexture('time0'))
-            let temp_length = this.cells.length
-            console.log(temp_length)
-            for(let i in this.cells){
-                this.cells.push(new Cell(this,  Math.floor(Math.random() * this.scale.width), Math.floor(Math.random() * this.scale.height), 'time0').setScale(0.5).setDepth(0))
-                console.log(this.cells.length, temp_length)
+        if(this.year<31){
+            if (this.time_in_cycle === 0){
+                //dissolve nuclear membrane
+                this.time_in_cycle++;
+                this.year++
+                this.cells.forEach((cell)=>cell.setTexture('time1'))
+                this.cells.forEach((cell)=>cell.clearPopup())
             }
-            this.cells.forEach((cell)=>cell.clearPopup())
+            else if (this.time_in_cycle === 1){
+                //centrosomes attach to 
+                // this.cell.setTexture('time1')
+                this.time_in_cycle++;
+                this.year++
+                this.cells.forEach((cell)=>cell.setTexture('time2'))
+                this.cells.forEach((cell)=>cell.clearPopup())
+            }
+            else if(this.time_in_cycle === 2){
+                //centrosomes pull apart chromosomes, cell squishes in middle
+                this.time_in_cycle++;
+                this.year++
+                this.cells.forEach((cell)=>cell.setTexture('time3'))
+                this.cells.forEach((cell)=>cell.clearPopup())
+            }
+            else if(this.time_in_cycle===3){
+                //nuclear membrane develops around each set of chromosomes, cell squishes more
+                this.time_in_cycle++;
+                this.year++
+                this.cells.forEach((cell)=>cell.setTexture('time4'))
+                this.cells.forEach((cell)=>cell.clearPopup())
+            }
+            else if(this.time_in_cycle===4){
+                //cell separates
+                this.time_in_cycle++;
+                this.year++
+                this.cells.forEach((cell)=>cell.setTexture('time5'))
+                this.cells.forEach((cell)=>cell.clearPopup())
+            }
+            else{
+                this.time_in_cycle=0;
+                this.year++
+                this.cells.forEach((cell)=>cell.setTexture('time0'))
+                let temp_length = this.cells.length
+                console.log(temp_length)
+                for(let i in this.cells){
+                    this.cells.push(new Cell(this,  Math.floor(Math.random() * this.scale.width), Math.floor(Math.random() * this.scale.height), 'time0').setScale(0.5).setDepth(0))
+                    console.log(this.cells.length, temp_length)
+                }
+                this.cells.forEach((cell)=>cell.clearPopup())
+            }
         }
         console.log(this.time_in_cycle, this.year)
-        if(this.year===30){
+        if(this.year>30){
             console.log('ive hit my limit')
+            new Textbox(this, 300,400, 'Max Limit Reached')
         }
     }
     create ()
