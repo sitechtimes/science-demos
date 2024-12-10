d45006d3218357e394fa3bb6845878e33b438201
Noahabbas80 <121987786+Noahabbas80@users.noreply.github.com>
2024-12-09 20:49:58 -0500

    im stupid

--- frontend/src/layout/AppFooter.vue
+++
@@ -1,8 +0,0 @@
-<script setup></script>
-
-<template>
-    <div class="layout-footer">
-        SITHS Science by
-        <p>Leo Dwi, Veronica Frid, Samuel Kipnis and Angelina Li</p>
-    </div>
-</template>
