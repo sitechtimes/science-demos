451737efef2d66f5186563fea3f2b979b0b54a91
noaha90 <121987786+Noahabbas80@users.noreply.github.com>
2024-12-12 12:01:53 -0500

    ,

---
+++ scienceDemo/Assets/graphScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class graphScript : MonoBehaviour
+{  
+    public static List<int[]> popData = new List<int[]>();
+    void Start(){
+        Debug.Log(popData);
+    }
+
+}
