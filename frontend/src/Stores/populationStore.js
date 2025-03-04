f2e4c8294112f6642d82380f7b395d265c9f3de5
LeoDwi <leod13579@gmail.com>
2025-03-04 18:51:36 -0500

    restructure

--- frontend/src/Stores/populationStore.js
+++ frontend/src/Stores/populationStore.js
@@ -3,103 +3,116 @@
 import { defineStore } from "pinia";
 
 export const populationStore = defineStore("populationStore", () => {
-  const species = ref({
-<<<<<<< Updated upstream
-    algae: {
+
+    const algae = ref({
       population: 27,
       img: path + 'algae.png',
       description: "blah blah blah blah blah",
-      history: {}
-    },
-    boulderStarCoral: {
+      history: {},
+      maxCapacity: [],
+      population: [] || 1
+    })
+    const boulderStarCoral = ref({
       population: 28, 
       img: path + 'boulderStarCoral.png',
       description: "blah blah blah blah blah",
-      history: {}
-    },
-    crownOfThornsStarfish: {
+      history: {},
+      maxCapacity: [],
+      population: [] || 1
+    })
+    const crownOfThornsStarfish = ref({
       population: 0, 
       img: path + 'crownOfThornsStarfish.png',
       description: "blah blah blah blah blah",
-      history: {}
-    },
-    hawksbillSeaTurtle: {
+      history: {},
+      maxCapacity: [],
+      population: [] || 1
+    })
+    const hawksbillSeaTurtle = ref({
       population: 25, 
       img: path + 'hawksbillSeaTurtle.png',
       description: "blah blah blah blah blah",
-      history: {}
-    },
-    longSpinedUrchin: {
+      history: {},
+      maxCapacity: [],
+      population: [] || 1
+    })
+    const longSpinedUrchin = ref({
       population: 1160, 
       img: path + 'longSpinedUrchin.png',
       description: "blah blah blah blah blah",
-      history: {}
-    },
-    nassauGrouper: {
+      history: {},
+      maxCapacity: [],
+      population: [] || 1
+    })
+    const nassauGrouper = ref({
       population: 48, 
       img: path + 'nassauGrouper.png',
       description: "blah blah blah blah blah",
-      history: {}
-    },
-    queenAngelfish: {
+      history: {},
+      maxCapacity: [],
+      population: [] || 1
+    })
+    const queenAngelfish = ref({
       population: 480, 
       img: path + 'queenAngelfish.png',
       description: "blah blah blah blah blah",
-      history: {}
-    },
-    redLionfish: {
+      history: {},
+      maxCapacity: [],
+      population: [] || 1
+    })
+    const redLionfish = ref({
       population: 0, 
       img: path + 'redLionfish.png',
       description: "blah blah blah blah blah",
-      history: {}
-    },
-    sponge: {
+      history: {},
+      maxCapacity: [],
+      population: [] || 1
+    })
+    const sponge = ref({
       population: 11, 
       img: path + 'sponge.png',
       description: "blah blah blah blah blah",
-      history: {}
-    },
-    spotlightParrotfish: {
+      history: {},
+      maxCapacity: [],
+      population: [] || 1
+    })
+    const spotlightParrotfish = ref({
       population: 825, 
       img: path + 'spotlightParrotfish.png',
       description: "blah blah blah blah blah",
-      history: {}
-    },
-    staghornCoral: {
+      history: {},
+      maxCapacity: [],
+      population: [] || 1
+    })
+    const staghornCoral = ref({
     population: 21, 
     img: path + 'staghornCoral.png',
     description: "blah blah blah blah blah",
-    history: {}
-    },
-    yellowtailSnapper: {
+    history: {},
+    maxCapacity: [],
+    population: [] || 1
+    })
+    const yellowtailSnapper= ref({
       population: 240, 
       img: path + 'yellowtailSnapper.png',
       description: "blah blah blah blah blah",
-      history: {}
-    }
-  })
+      history: {},
+      maxCapacity: [],
+      population: [] || 1
+    })
 
-  function getValue(type) {
-    return species[type]
-  }
-  function setValue(type, value) {
-    species[type] = value
-  }
-=======
-    staghornCoral: {
-      population: 20,
-      species: "Invasive",
-      info: "blah blah blah blah blah",
-      isPresent: false,
-      history: {}
-    },
-    redLionfish: {
-      population: 20,
-      species: "Invasive",
-      info: "blah blah blah blah blah",
-      isPresent: false,
-      history: {}
-    }
-  })
->>>>>>> Stashed changes
+    return {
+        algae,
+        boulderStarCoral,
+        crownOfThornsStarfish,
+        hawksbillSeaTurtle,
+        longSpinedUrchin,
+        nassauGrouper,
+        queenAngelfish,
+        redLionfish,
+        sponge,
+        spotlightParrotfish,
+        staghornCoral,
+        yellowtailSnapper
+    };
 });
