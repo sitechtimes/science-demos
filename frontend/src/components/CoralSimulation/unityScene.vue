b158534aefe6934f94043f18201d7f4f7e9cce5a
Izzy-Zoltan <izakzoltan@gmail.com>
2025-05-06 11:00:20 -0400

    updated unity

--- frontend/src/components/CoralSimulation/unityScene.vue
+++ frontend/src/components/CoralSimulation/unityScene.vue
@@ -11,10 +11,10 @@ import UnityVue from "unity-webgl/vue";
 const cacheBuster = Date.now(); // Unique number each load
 
 const unityContext = new UnityWebgl({
-  loaderUrl: `/Build/Coral/Downloads.loader.js?v=${cacheBuster}`,
-  dataUrl: `/Build/Coral/Downloads.data?v=${cacheBuster}`,
-  frameworkUrl: `/Build/Coral/Downloads.framework.js?v=${cacheBuster}`,
-  codeUrl: `/Build/Coral/Downloads.wasm?v=${cacheBuster}`,
+  loaderUrl: `/Build/Coral/CoralReef.loader.js?v=${cacheBuster}`,
+  dataUrl: `/Build/Coral/CoralReef.data?v=${cacheBuster}`,
+  frameworkUrl: `/Build/Coral/CoralReef.framework.js?v=${cacheBuster}`,
+  codeUrl: `/Build/Coral/CoralReef.wasm?v=${cacheBuster}`,
 });
 
 unityContext
